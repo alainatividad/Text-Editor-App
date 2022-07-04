afd2833aac9ae852cbe48dc2eacbf254e4ef6c70
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-05 00:09:07 +1000

    Added code to save text in indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Post to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  // let's try to save it on id 1 first
+  let request = store.put({ id: 1, value: content });
+  let result = await request;
+  // if the put fails, add
+  if (!result) {
+    request = store.add({ value: content });
+    result = await request;
+  }
+  console.log("🚀 - data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  let request = store.count();
+  let result = await request;
+  if (result) {
+    return await store.get(1)[0].value;
+  }
+  return result;
+};
 
 initdb();
