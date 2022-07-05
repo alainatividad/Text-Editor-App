7df0810a4955fc35fe51c5221e85f4ef5d2f44b2
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-05 22:01:36 +1000

    Fixed code + added comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,25 +3,29 @@ import { openDB } from "idb";
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
+      // check if the indexedDB already contains the database jate
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
+      // if the database doesn't exist, create jate then set the keypath to id and set it to autoincrement
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
+      // let's initialise the database by saving a blank record in it.
+      putDb(" ");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("Post to the database");
+  // open the database, start a readwrite transaction, obtain the object store then finally modify the content of the first entry
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  // let's try to save it on id 1 first
+  // let's try to save it on id 1 first, modifying the value of content
   let request = store.put({ id: 1, value: content });
   let result = await request;
-  // if the put fails, add
+  // if the put fails, add a new record instead
   if (!result) {
     request = store.add({ value: content });
     result = await request;
@@ -29,18 +33,15 @@ export const putDb = async (content) => {
   console.log("🚀 - data saved to the database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
+  // open the database, start a readonly transaction, obtain the object store then finally get the content of the first entry
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  let request = store.count();
-  let result = await request;
-  if (result) {
-    return await store.get(1)[0].value;
-  }
-  return result;
+  // return the first entry of the result
+  return await store.get(1)[0];
 };
 
+// initialise database
 initdb();
